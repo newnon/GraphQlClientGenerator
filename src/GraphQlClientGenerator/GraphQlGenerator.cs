ee28f34791abc145debadf989efa3d4ed980ad51
s_perepelitsa <s.perepelitsa@spherelab.ru>
2019-08-29 16:28:16 +0300

    DateTime support

--- src/GraphQlClientGenerator/GraphQlGenerator.cs
+++ src/GraphQlClientGenerator/GraphQlGenerator.cs
@@ -321,6 +321,9 @@ using Newtonsoft.Json.Linq;
                         case GraphQlTypeBase.GraphQlTypeScalarId:
                             propertyType = GetIdNetType();
                             break;
+                        case GraphQlTypeBase.GraphQlTypeScalarDateTime:
+                            propertyType = "DateTime?";
+                            break;
                         default:
                             propertyType = GetCustomScalarType(baseType, fieldType, member.Name);
                             break;
